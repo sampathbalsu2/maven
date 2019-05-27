3b1b966eba0ccaf00b8b4d96f96e37b961b89b49
test <test@10.154.74.155>
2019-05-27 12:58:06 +0530

    added testng.xml

---
+++ work/src/test/java/regression/Locatorstest.java
@@ -0,0 +1,28 @@
+package regression;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+public class Locatorstest {
+	WebDriver dr;
+  @Test
+  public void f() {
+	  dr.get("https://www.google.com");
+	  dr.findElement(By.cssSelector(".gLFyf.gsfi")).sendKeys("Hi");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\test\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	  dr= new ChromeDriver();
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  dr.quit();
+  }
+
+}
